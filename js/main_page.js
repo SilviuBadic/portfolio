0bbc425e24f9701d5bc77fa619aac2abdd3a375a
Silviu <silviu_nicholas@yahoo.com>
2023-12-18 21:27:32 +0200

    first_commit

---
+++ js/main_page.js
@@ -0,0 +1,77 @@
+let itImage = document.getElementById("it_image");
+
+function myFunction() {
+  let x = document.getElementById("skillsOne");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+    itImage.style.marginLeft ="200px";
+    itImage.style.transitionDuration = "1s";
+    itImage.style.transitionDelay = "0.2s";
+    itImage.style.rotate = "5deg";
+    
+  }
+  
+}
+
+function myFunction2() {
+  let x = document.getElementById("skillsOne");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+    itImage.style.marginLeft ="0px";
+    itImage.style.rotate = "0deg";
+  }
+}
+
+function myFunction3() {
+  let x = document.getElementById("skillsTwo");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+    
+  } else {
+    x.style.display = "block";
+    x.style.marginLeft = "550px";
+    itImage.style.opacity = "0.1";
+  }
+    
+  }
+  
+
+function myFunction4() {
+  let x = document.getElementById("skillsTwo");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+    itImage.style.marginLeft ="0px"
+    itImage.style.opacity = "1";
+  }
+}
+
+
+function myFunction5() {
+  let x = document.getElementById("myHobbies");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+    x.style.paddingLeft = "1050px";
+    itImage.style.marginLeft ="-200px";
+    itImage.style.rotate = "-5deg";
+  }
+}
+
+function myFunction6() {
+  let x = document.getElementById("myHobbies");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+    itImage.style.marginLeft ="0px"
+    itImage.style.opacity = "1";
+    itImage.style.rotate = "0deg";
+  }
+}
